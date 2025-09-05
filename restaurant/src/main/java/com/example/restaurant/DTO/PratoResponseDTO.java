89ca4a242fe67aa1939785ea56f6c547049e8379
Luiz Otávio de Almeida <luizotavioodealemida@gmail.com>
2025-09-04 22:10:09 -0300

    Update PratoResponseDTO.java

--- restaurant/src/main/java/com/example/restaurant/DTO/PratoResponseDTO.java
+++ restaurant/src/main/java/com/example/restaurant/DTO/PratoResponseDTO.java
@@ -1,5 +1,43 @@
 package com.example.restaurant.DTO;
 
-public class PratoResponseDTO {
+
+
+public class PratoRequestDTO {
+    private String nome;
+    private double preco;
+    private boolean disponivel;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+
+    public PratoRequestDTO() {
+    }
+
+    public PratoRequestDTO(String nome, double preco, boolean disponivel) {
+        this.nome = nome;
+        this.preco = preco;
+        this.disponivel = disponivel;
+    }
 
 }
